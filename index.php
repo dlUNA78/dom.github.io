2558aadf681bc664c2643ca77616d993044f6b4d
dlUNA78 <167263733+dlUNA78@users.noreply.github.com>
2025-05-29 18:33:46 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+  <title>Home - Brand</title>
+  <!-- CSS -->
+<link rel="stylesheet" href="Views/assets/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" href="Views/assets/fonts/font-awesome.min.css" />
+<link rel="stylesheet" href="Views/assets/fonts/ionicons.min.css" />
+<link rel="stylesheet" href="Views/assets/fonts/simple-line-icons.min.css" />
+<link rel="stylesheet" href="Views/assets/css/bs-theme-overrides.css" />
+<link rel="stylesheet" href="Views/assets/css/Animated-Pretty-Product-List-v12-Animated-Pretty-Product-List.css" />
+<link rel="stylesheet" href="Views/assets/css/baguetteBox.min.css" />
+<link rel="stylesheet" href="Views/assets/css/Banner-Heading-Image-images.css" />
+<link rel="stylesheet" href="Views/assets/css/dark_navbar.css" />
+<link rel="stylesheet" href="Views/assets/css/Dark-NavBar-Navigation-with-Button.css" />
+<link rel="stylesheet" href="Views/assets/css/Dark-NavBar-Navigation-with-Search.css" />
+<link rel="stylesheet" href="Views/assets/css/Footer---4-Columns---No-Social-Networks.css" />
+<link rel="stylesheet" href="Views/assets/css/Footer-Clean-icons.css" />
+<link rel="stylesheet" href="Views/assets/css/multiple-item-carousel-slider.css" />
+<link rel="stylesheet" href="Views/assets/css/vanilla-zoom.min.css" />
+
+<!-- JS -->
+<script src="Views/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="Views/assets/js/baguetteBox.min.js"></script>
+<script src="Views/assets/js/multiple-item-carousel-slider-multiple-item-carousel-slider-bootstrap.min.js"></script>
+<script src="Views/assets/js/vanilla-zoom.js"></script>
+<script src="Views/assets/js/multiple-item-carousel-slider-multiple-item-carousel-slider-slider.js"></script>
+<script src="Views/assets/js/Theme_Prin.js"></script>
+
+</head>
+<body>
+  <?php
+  include __DIR__ . '/Views/Paginas Principales/menu.php';
+
+
+  ?>
+  <main class="page" style="background: rgb(255, 242, 205)">
+    <section class="clean-block clean-hero" style="
+          background-image: url('./Views/assets/img/tech/imange4.webp');
+          color: rgba(88, 122, 46, 0.55);
+        ">
+      <div class="text">
+        <h2 style="
+              color: rgb(255, 255, 255);
+              font-family: 'ADLaM Display', serif;
+            ">
+          SATELITE COMUNICACIONES
+        </h2>
+        <p style="
+              color: rgb(249, 251, 246);
+              font-family: 'ADLaM Display', serif;
+            ">
+          Soluciones especializadas en radiocomunicación para hogares,
+          vehículos, control de plagas y sistemas de&nbsp;energía&nbsp;solar
+        </p>
+      </div>
+    </section>
+    <section class="justify-content-center align-items-center clean-block"
+      style="background: #fff2cdb3; padding-top: 12px; padding-bottom: 0px">
+      <div class="container" style="padding-right: 12px; padding-top: 0px; padding-bottom: 14px">
+        <div class="block-heading" style="border-color: rgb(63, 104, 145)">
+          <h2 style="
+                color: rgb(88, 122, 46);
+                font-family: 'ADLaM Display', serif;
+              ">
+            En nuestra tienda puede encontrar
+          </h2>
+        </div>
+        <div class="row justify-content-center" style="margin-right: -14px; margin-left: 0px">
+          <div class="col-md-5 feature-box">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2"
+              stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"
+              class="icon icon-tabler icon-tabler-antenna icon" style="color: rgb(88, 122, 46)">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M20 4v8"></path>
+              <path d="M16 4.5v7"></path>
+              <path d="M12 5v16"></path>
+              <path d="M8 5.5v5"></path>
+              <path d="M4 6v4"></path>
+              <path d="M20 8h-16"></path>
+            </svg>
+            <a class="text-decoration-none" href="/p/Views/Categorias/categoria_prod.php?categoria=TV%20satelital">
+              <div>
+                <h4 class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                    ">
+                  <strong><span style="color: rgb(33, 37, 41)">TV satelitalz</span></strong>
+                </h4>
+                <p class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                      text-align: justify;
+                    ">
+                  ¡Vive la mejor experiencia en TV vía satélite con VTV y Star
+                  TV! Disfruta los canales más vistos en México, deportes en
+                  vivo y entretenimiento familiar sin&nbsp;interrupciones.
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-5 feature-box">
+            <i class="icon-microphone icon" style="color: rgb(88, 122, 46)"></i>
+            <a class="text-decoration-none" href="/p/Views/Categorias/categoria_prod.php?categoria=Radiocomunicación">
+              <div>
+                <h4 class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                    ">
+                  <strong><span style="color: rgb(33, 37, 41)">Radiocomunicación&nbsp;</span></strong>
+                </h4>
+                <p class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                      text-align: justify;
+                    ">
+                  Mantente siempre comunicado con nuestros equipos de
+                  radiocomunicación. Radios portátiles, móviles y de base para
+                  mejorar el desempeño en tu trabajo con señal clara y
+                  confiable. ¡Comunicación&nbsp;sin&nbsp;límites
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-5 feature-box">
+            <i class="icon ion-ios-home icon" style="color: rgb(88, 122, 46)"></i>
+            <a class="text-decoration-none" href="/p/Views/Categorias/categoria_prod.php?categoria=Hogar">
+              <div>
+                <h4 class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                    ">
+                  <strong><span style="color: rgb(33, 37, 41)">Hogar</span></strong>
+                </h4>
+                <p class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                      text-align: justify;
+                    ">
+                  Equipa tu hogar con tradición y calidad. Encuentra metates,
+                  molinos, bateas, molcajetes, extractores de jugo, comales y
+                  quemadores. Todo lo que necesitas para cocinar con
+                  auténtico&nbsp;sabor
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-5 feature-box">
+            <i class="icon ion-android-car icon" style="color: rgb(88, 122, 46)"></i>
+            <a class="text-decoration-none" href="/p/Views/Categorias/categoria_prod.php?categoria=Automotriz">
+              <div>
+                <h4 class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                    ">
+                  <strong><span style="color: rgb(33, 37, 41)">Automotriz</span></strong>
+                </h4>
+                <p class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                      text-align: justify;
+                    ">
+                  Mantén tu vehículo en óptimas condiciones con nuestra línea
+                  automotriz. Acumuladores, lubricantes, bujías y cámaras para
+                  autos y motocicletas. ¡Calidad y rendimiento
+                  para&nbsp;cada&nbsp;viaje!
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-5 feature-box">
+            <i class="icon ion-bug icon" style="color: rgb(88, 122, 46)"></i>
+            <a class="text-decoration-none" href="/p/Views/Categorias/categoria_prod.php?categoria=Plaguicidas">
+              <div>
+                <h4 class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                    ">
+                  <strong>Plaguicidas&nbsp;</strong>
+                </h4>
+                <p class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                      text-align: justify;
+                    ">
+                  Protege tu hogar, jardín y cultivos con los mejores
+                  plaguicidas. Ofrecemos soluciones efectivas para el control
+                  de maleza y plagas en espacios urbanos y agropecuarios.
+                  ¡Eficiencia y seguridad&nbsp;garantizadas!
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-5 feature-box">
+            <a class="text-decoration-none" href="/p/Views/Categorias/categoria_prod.php?categoria=Energia%20solar">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                  class="icon icon-tabler icon-tabler-artboard icon" style="color: rgb(88, 122, 46)">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M8 8m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z"></path>
+                  <path d="M3 8l1 0"></path>
+                  <path d="M3 16l1 0"></path>
+                  <path d="M8 3l0 1"></path>
+                  <path d="M16 3l0 1"></path>
+                  <path d="M20 8l1 0"></path>
+                  <path d="M20 16l1 0"></path>
+                  <path d="M8 20l0 1"></path>
+                  <path d="M16 20l0 1"></path>
+                </svg>
+                <h4 class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                    ">
+                  Energia solar
+                </h4>
+                <p class="text-decoration-none" style="
+                      color: rgb(0, 0, 0);
+                      font-family: 'ADLaM Display', serif;
+                      text-align: justify;
+                    ">
+                  Aprovecha el poder del sol con nuestros calentadores de agua
+                  solares y paneles solares para iluminación. Ahorra energía y
+                  cuida el planeta con soluciones ecológicas y eficientes.
+                  ¡Energía&nbsp;gratis
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="justify-content-center align-items-center clean-block" style="
+      background: #fff2cdb3;
+      padding: 0px 0px 0px;
+      padding-top: 0px;
+      padding-bottom: 0px;
+    ">
+      <!-- inicia tabla de ofertas -->
+      <div id="ofertas-container" class="row justify-content-center my-5">
+        <div class="col">
+          <h3 class="text-center mb-4" style="color: #587a2e; font-family: 'ADLaM Display', serif;">Ofertas</h3>
+          <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <?php
+
+                include  __DIR__ . '/config/database.php';
+              $result = $conn->query("SELECT * FROM ofertas WHERE CURDATE() BETWEEN Fecha_inicio AND Fecha_expirada");
+              if ($result && $result->num_rows > 0) {
+                $active = true;
+                while ($row = $result->fetch_assoc()) {
+                  $imagen_relativa = ltrim($row['imagen'], '/');
+                  $absolute_image_path = $_SERVER['DOCUMENT_ROOT'] . '/' . $imagen_relativa;
+                  $web_image_path = '/' . $imagen_relativa;
+
+                  if (!file_exists($absolute_image_path) || empty($row['imagen'])) {
+                    $web_image_path = '/assets/img/default-product.jpg';
+                  }
+
+                  $descuento = 0;
+                  if ($row['precio'] > 0) {
+                    $descuento = round((($row['precio'] - $row['precio_oferta']) / $row['precio']) * 100);
+                  }
+                  ?>
+                  <div class="carousel-item <?= $active ? 'active' : '' ?>">
+                    <div class="card mx-auto" style="width: 350px; border-radius: 20px; cursor: pointer;"
+                      onclick="window.location.href='./Views/Paginas Principales/detalle_oferta.php?id=<?= (int) $row['id_oferta'] ?>'">
+                      <div class="position-absolute bg-danger text-white px-3 py-1"
+                        style="border-bottom-right-radius: 15px; font-weight: bold; z-index: 1; top: 0; left: 0;">
+                        -<?= $descuento ?>%
+                      </div>
+                      <div class="img-wrapper">
+                        <img src="<?= htmlspecialchars($web_image_path) ?>"
+                          alt="<?= htmlspecialchars($row['Nombre_oferta']) ?>"
+                          style="width: 100%; height: 200px; object-fit: contain; border-radius: 20px 20px 0 0;"
+                          onerror="this.onerror=null;this.src='/assets/img/default-product.jpg';">
+                      </div>
+                      <div class="card-body">
+                        <h5 class="card-title" style="font-family: 'ADLaM Display', serif; color: #587a2e;">
+                          <?= htmlspecialchars($row['Nombre_oferta']) ?>
+                        </h5>
+                        <p class="card-text"
+                          style="font-family: 'AR One Sans', sans-serif; text-align: justify; color: #6c757d;">
+                          <?= htmlspecialchars($row['descripcion']) ?>
+                        </p>
+                        <div class="d-flex align-items-center">
+                          <span class="text-muted text-decoration-line-through me-3">
+                            $<?= number_format($row['precio'], 2) ?>
+                          </span>
+                          <span class="text-success fw-bold fs-4">
+                            $<?= number_format($row['precio_oferta'], 2) ?>
+                          </span>
+                        </div>
+                        <small class="text-muted d-block mt-2">
+                          Válido hasta: <?= date('d/m/Y', strtotime($row['Fecha_expirada'])) ?>
+                        </small>
+                      </div>
+                    </div>
+                  </div>
+                  <?php
+                  $active = false;
+                }
+              } else {
+                echo "<script>document.getElementById('ofertas-container').style.display = 'none';</script>";
+              }
+              $conn->close();
+              ?>
+            </div>
+            <!-- Controles del carrusel -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+              data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+              data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Siguiente</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- termina tabla de ofertas -->
+    </section>
+    <br>
+    <br>
+    <!-- inicia footer -->
+    <?php include (__DIR__).'/Views/Paginas Principales/footer_principal.php'; ?>
+    <!-- termina footer -->
+  </main>
+  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+  <script src="../assets/js/baguetteBox.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/js/multiple-item-carousel-slider-multiple-item-carousel-slider-bootstrap.min.js"></script>
+  <script src="../assets/js/vanilla-zoom.js"></script>
+  <script src="../assets/js/multiple-item-carousel-slider-multiple-item-carousel-slider-slider.js"></script>
+  <script src="../assets/js/Theme_Prin.js"></script>
+  <script src="../assets/js/Animated-Pretty-Product-List-v12-Animated-Pretty-Product-List.js"></script>
+</body>
+</html>
